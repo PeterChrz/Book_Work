ea13ee10394f80f516a1b386cccd4c4ca9303cda
PeterChrz <pc@stampedepress.org>
2023-12-29 08:22:39 -0500

    C - bool and vars

---
+++ Beejs_Guide_to_C/Ch03/bool.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+	bool x = true;
+	int y = 1;
+
+	if (x) {
+	    printf("x is true!\n");
+	}
+
+	if (y) {
+	    printf("y is true!\n");
+	}
+}
---
+++ Beejs_Guide_to_C/Ch03/vars.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main(void)
+{
+    int i = 2;
+    float f = 3.14;
+    char *s = "Hello World!"; // char * ("Char pointer") is the string type.
+
+    printf("%s i = %d and f = %f!\n", s, i, f); //Order of vars doesn't matter but is reccomended for readability.
+
+}
