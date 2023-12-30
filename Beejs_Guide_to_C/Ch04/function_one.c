76e1891d0ce456b2011ba3c23c952e0a9c11ed71
PeterChrz <pc@stampedepress.org>
2023-12-30 14:16:51 -0500

    C - Functions

---
+++ Beejs_Guide_to_C/Ch04/function_one.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int plus_one(int n) // the "Definition"
+{
+    //The int before plus_one indicates the return type.
+    //(int n) indicates this function takes one int argument stored in parameter n.
+    // A parameter is a special type of local variable into which args are copied.
+    return n + 1;
+
+}
+
+
+// We can then call this function in main() and assign it to local var j.
+// Functions must all come before main().
+
+int main(void) {
+
+    int i = 10, j;
+
+    j = plus_one(i); //the "call"
+
+    printf("i + 1 is %d\n", j);
+}
---
+++ Beejs_Guide_to_C/Ch04/void.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// This function takes no args and returns no value.
+
+void hello(void) {
+    printf("Hello, world!\n");
+}
+
+int main(void) {
+
+    hello(); //Prints hello world.
+}
